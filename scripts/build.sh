a1a374353558f7688d36628e670f3693e5ea3098
Valentine Reese <vreese177@gmail.com>
2023-03-24 15:41:54 +0800

    Add script for build

--- scripts/build.sh
+++ scripts/build.sh
@@ -7,5 +7,17 @@ set -e
 
 SCRIPT_PATH="$( cd "$( dirname "$0" )" >/dev/null 2>&1 && pwd )"
 
-cd "$SCRIPT_PATH/../poly-crosschain" || exit
+cd "$SCRIPT_PATH/../poly-cross-chain" || exit
+mpm package build --doc --abi --force
+
+cd "$SCRIPT_PATH/../stc-assets" || exit
+mpm package build --doc --abi --force
+
+cd "$SCRIPT_PATH/../stc-smt" || exit
+mpm package build --doc --abi --force
+
+cd "$SCRIPT_PATH/../stc-utils" || exit
+mpm package build --doc --abi --force
+
+cd "$SCRIPT_PATH/../zion-cross-chain" || exit
 mpm package build --doc --abi --force
--- scripts/pre_release.sh
+++ scripts/pre_release.sh
@@ -1,6 +1,20 @@
 #!/bin/bash
 
-mpm package build
-mpm package test
-mpm integration-test
+set -e
+
+SCRIPT_PATH="$( cd "$( dirname "$0" )" >/dev/null 2>&1 && pwd )"
+
+cd "$SCRIPT_PATH/../poly-cross-chain" || exit
+mpm release
+
+cd "$SCRIPT_PATH/../stc-assets" || exit
+mpm release
+
+cd "$SCRIPT_PATH/../stc-smt" || exit
+mpm release
+
+cd "$SCRIPT_PATH/../stc-utils" || exit
+mpm release
+
+cd "$SCRIPT_PATH/../zion-cross-chain" || exit
 mpm release
