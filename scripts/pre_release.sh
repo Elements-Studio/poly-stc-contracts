f5dbbd5ea670a38564583f11cd84018255d63d45
Valentine Reese <vreese177@gmail.com>
2023-04-03 09:49:07 +0800

    Pre release script commit

--- scripts/pre_release.sh
+++ scripts/pre_release.sh
@@ -5,16 +5,27 @@ set -e
 SCRIPT_PATH="$( cd "$( dirname "$0" )" >/dev/null 2>&1 && pwd )"
 
 cd "$SCRIPT_PATH/../poly-cross-chain" || exit
+mpm package test
 mpm release
+mpm integration-test -p ./
 
 cd "$SCRIPT_PATH/../stc-assets" || exit
+mpm package test
+mpm integration-test -p ./
 mpm release
 
+
 cd "$SCRIPT_PATH/../stc-smt" || exit
+mpm package test
+mpm integration-test -p ./
 mpm release
 
 cd "$SCRIPT_PATH/../stc-utils" || exit
+mpm package test
+mpm integration-test -d ./
 mpm release
 
 cd "$SCRIPT_PATH/../zion-cross-chain" || exit
+mpm package test
+mpm integration-test -d ./
 mpm release
